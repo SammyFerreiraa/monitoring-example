49118eef0cfc7b398d2895edad08e0fcb5f9738c
Sammy Ferreira <sammyfe2021@gmail.com>
2023-10-12 16:16:59 -0300

    :sparkles: feat: add new types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,4 +1,28 @@
 export type LabelInputProps = {
   label: string
   type: string
+  htmlFor?: string | undefined
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+export type UserProps = {
+  email: string
+  password: string
+  name: string
+  age: number
+  monitor: boolean
+  monitoring_disciplines: string[]
+  disciplines: string[]
+}
+
+export type AllUsers = {
+  users: UserProps[]
+}
+
+export interface NextAuthSessionProviderProps {
+  children: React.ReactNode
+}
+
+export interface PrivateLayoutProps {
+  children: React.ReactNode
 }
