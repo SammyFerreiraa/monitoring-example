e9213e0f2255a9853e3a819749054d379f5a3b2b
Sammy Ferreira <sammyfe2021@gmail.com>
2023-10-12 16:16:29 -0300

    :bug: fix: corrections input and label component

--- src/components/form/LabelInput.tsx
+++ src/components/form/LabelInput.tsx
@@ -2,11 +2,17 @@ import { LabelInputProps } from '@/types'
 import { Input } from '../ui/input'
 import { Label } from '../ui/label'
 
-const LabelInput = ({ label, type }: LabelInputProps) => {
+const LabelInput = ({ label, type, onChange, htmlFor }: LabelInputProps) => {
   return (
     <div>
-      <Label>{label}</Label>
-      <Input type={type} />
+      <Label htmlFor={htmlFor}>{label}</Label>
+      <Input
+        id={htmlFor}
+        autoComplete="off"
+        name={htmlFor}
+        type={type}
+        onChange={onChange}
+      />
     </div>
   )
 }
