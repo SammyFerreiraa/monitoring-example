43bcc00d801c3ed6a95122dc343ec4bfcfff7857
Sammy Ferreira <sammyfe2021@gmail.com>
2023-10-17 14:47:22 -0300

    :sparkles: feat: return callback

--- src/templates/dashboard/Dashboard.tsx
+++ src/templates/dashboard/Dashboard.tsx
@@ -1,5 +1,12 @@
-const Dashboard = () => {
-  return <div>Você esta logado!</div>
+import { ButtonLogout } from '@/components'
+
+const Dashboard = async () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center gap-4">
+      <p>Olá, Você esta logado!</p>
+      <ButtonLogout />
+    </main>
+  )
 }
 
 export default Dashboard
---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,9 @@
+import NextAuth from 'next-auth/next'
+
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      name: string
+    }
+  }
+}
