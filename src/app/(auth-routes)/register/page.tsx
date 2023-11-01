a396b591dcc3fb398cc71d4f9f2b17db7337d498
Sammy Ferreira <sammyfe2021@gmail.com>
2023-11-01 12:54:12 -0300

    :sparkles: feat: initial register page

---
+++ src/app/(auth-routes)/register/page.tsx
@@ -0,0 +1,11 @@
+import { RegisterPage } from "@/templates"
+
+const page = () => {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <RegisterPage />
+    </main>
+  )
+}
+
+export default page
--- src/templates/index.ts
+++ src/templates/index.ts
@@ -1,3 +1,4 @@
 export { default as Dashboard } from './dashboard/Dashboard'
 export { default as LoginPage } from './login/LoginPage'
 export { default as Homepage } from './home-page/Homepage'
+export { default as RegisterPage } from './register/RegisterPage'
